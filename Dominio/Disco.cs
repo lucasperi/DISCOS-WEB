62226e619c6d30ea5cb125da995d9aad5251847c
lucasperi <lucas05peri@gmail.com>
2023-08-30 11:01:22 -0300

    Creación del Dominio

---
+++ Dominio/Disco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Disco
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public SqlDateTime FechaLanzamiento { get; set; }
+
+        public int CantidadCanciones { get; set; }
+
+        public string UrlImagenTapa { get; set; }
+
+        public Estilo Estilo { get; set; }
+
+        public TipoEdicion TipoEdicion { get; set; }
+
+    }
+}
